7f38c1e261aceb0d09aaa5c87e7e3f4a0f2c67bd
crayon <1657653340@qq.com>
2019-05-08 09:27:22 +0800

    优化代码

--- server/src/test/java/com/ucd/server/ServerApplicationTests.java
+++ server/src/test/java/com/ucd/server/ServerApplicationTests.java
@@ -1,9 +1,11 @@
 package com.ucd.server;
 
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -11,6 +13,8 @@ public class ServerApplicationTests {
 
     @Test
     public void contextLoads() {
+
     }
+
 
 }
