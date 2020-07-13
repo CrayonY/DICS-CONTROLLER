9b97ed571c364527f5643b20f1defcaa09611a57
crayon <1657653340@qq.com>
2020-07-13 10:04:24 +0800

    review 代码

--- server/src/main/java/com/ucd/server/exception/ExceptionEnum.java
+++ server/src/main/java/com/ucd/server/exception/ExceptionEnum.java
@@ -1,25 +1,27 @@
 package com.ucd.server.exception;
+
 /**
  * 异常枚举类
- * <p>Title: ExceptionEnum</p>  
- * @author  lx  
- * @date    
+ * <p>Title: ExceptionEnum</p>
+ *
+ * @author lx
+ * @date
  */
 public enum ExceptionEnum {
-	
-	ERROR_NOFOUND("无法找到相应的数据");
-	
-	private String value;
- 
-	public String getValue() {
-		return value;
-	}
- 
-	ExceptionEnum(String value){
-		this.value = value;
-	}
-	
-	public String toString() {
-		return value;
-	}
+
+    ERROR_NOFOUND("无法找到相应的数据");
+
+    private String value;
+
+    public String getValue() {
+        return value;
+    }
+
+    ExceptionEnum(String value) {
+        this.value = value;
+    }
+
+    public String toString() {
+        return value;
+    }
 }
